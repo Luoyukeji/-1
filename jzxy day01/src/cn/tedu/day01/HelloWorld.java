fffc1c7cb032381d3c0f7971e202ec273647a6c0
hmmwx53 <2085324623@qq.com>
2022-07-16 15:16:50 +0800

    hello world

---
+++ jzxy day01/src/cn/tedu/day01/HelloWorld.java
@@ -0,0 +1,24 @@
+package cn.tedu.day01;
+/*
+Ctrl+ s 保存
+Ctrl+ a 全选
+eclipse快捷键
+syso  alt+/  提示补全
+Ctrl + z 撤销
+Ctrl + x 裁剪
+Ctrl + c复制
+Ctrl +v 粘贴
+Ctrl +i 格式化代码//Ctrl alt + i
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("HelloWorld");//打印并换行
+        System.out.println();
+        System.out.print("今天是个下雨天");//打印不换行
+        System.out.print("只待在家");
+        System.out.println();
+        System.out.println("明天是个大晴天");
+        System.out.println("可以出门打篮球");
+    }
+}
